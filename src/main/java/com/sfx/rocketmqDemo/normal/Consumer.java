434961d1e9f3e6f07d8fd3950b04bb4786535035
李志强 <lizhiqiang1@msunsoft.com>
2019-11-11 13:09:03 +0800

    ip change

--- src/main/java/com/sfx/rocketmqDemo/normal/Consumer.java
+++ src/main/java/com/sfx/rocketmqDemo/normal/Consumer.java
@@ -10,21 +10,29 @@ import org.apache.rocketmq.common.message.MessageExt;
 import java.util.List;
 
 public class Consumer {
+    static final String NAMESRV_ADDR="10.68.5.195:9876";
     public static void main(String[] args) throws MQClientException {
         //1.创建消费者对象并指定消费者组名
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
         //2.设置NameSrv地址
-        consumer.setNamesrvAddr("192.168.253.152:9876");
+        consumer.setNamesrvAddr(NAMESRV_ADDR);
         //3.订阅消息并绑定监听处理消息
         consumer.subscribe("topic1","tag1");
         consumer.setMessageListener(new MessageListenerConcurrently() {
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                System.out.println(msgs);
+                for (MessageExt msg :msgs) {
+
+                    System.out.print(new String(msg.getBody())+",");
+                    System.out.println(msg);
+
+
+                }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
         //4.启动
         consumer.start();
+        System.out.println("消费者启动成功");
 
    }
 
--- src/main/java/com/sfx/rocketmqDemo/normal/SyncProducer.java
+++ src/main/java/com/sfx/rocketmqDemo/normal/SyncProducer.java
@@ -8,19 +8,21 @@ import org.apache.rocketmq.common.message.Message;
  * 同步生产者，生产信息
  */
 public class SyncProducer {
+    static final String NAMESRV_ADDR="10.68.5.195:9876";
     public static void main(String[] args) throws Exception {
         //1.创建生产者对象并制定生产者组名
         DefaultMQProducer producer =  new DefaultMQProducer("group1");
         //2.设置namesrv的地址
-        producer.setNamesrvAddr("192.168.253.152:9876");
+        producer.setNamesrvAddr(NAMESRV_ADDR);
         //3.启动producer
         producer.start();
         //4.创建消息内容并发送消息
         for (int i = 0; i < 3; i++) {
-            String msgStr = "hello_world" +i;
+            String msgStr = "欢迎来到rocketmq" +i;
             Message msg = new Message("topic1","tag1",msgStr.getBytes());
             SendResult result = producer.send(msg);
             System.out.println(result);
+            //Thread.sleep(1000);
         }
         //5.关闭producer
         producer.shutdown();
